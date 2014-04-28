2b52b361054147d496fbecc0dee3d39550ea365a
Stephen Beeman <stephen@gizmocracy.com>
2014-04-28 16:58:57 -0700

    Created

---
+++ DebugManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugManager : Singleton<DebugManager>
+{
+	#region Singleton<T> boilerplate
+	void Awake()
+	{
+		// You MUST call the base class onAwake() method
+		//	before you exit Awake().
+		onAwake();
+
+		/*
+		 * Do not exit Awake() without being ready to
+		 *	receive calls to all your methods. You cannot
+		 *	rely on references to other objects until Start()
+		 *	is called, but because Unity's order of initialization
+		 *	is not defined within a scene, your singleton
+		 *	may receive calls from other objects in between
+		 *	Awake() and Start().
+		 */
+	}
+	#endregion
+
+	public void Log(string message, params object[] fields)
+	{
+		if (!GlobalsManager.Instance.UseLogging)
+			return;
+
+		string actual = "oops";
+		if (fields == null || fields.Length == 0)
+			actual = message;
+		else
+			actual = string.Format(message, fields);
+
+		Debug.Log (actual);
+	}
+}
---
+++ GlobalsManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalsManager : Singleton<GlobalsManager>
+{
+	#region Singleton<T> boilerplate
+	void Awake()
+	{
+		// You MUST call the base class onAwake() method
+		//	before you exit Awake().
+		onAwake();
+		
+		/*
+		 * Do not exit Awake() without being ready to
+		 *	receive calls to all your methods. You cannot
+		 *	rely on references to other objects until Start()
+		 *	is called, but because Unity's order of initialization
+		 *	is not defined within a scene, your singleton
+		 *	may receive calls from other objects in between
+		 *	Awake() and Start().
+		 */
+
+		UseLogging = LogAlways || Debug.isDebugBuild;
+	}
+	#endregion
+
+
+	public bool LogAlways = false;
+
+	public bool UseLogging { get; protected set; }
+}
